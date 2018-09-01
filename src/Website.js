debc6676dcbe94b07c9759e3a135b64025ce65c4
void <ryanmckeown82@gmail.com>
2018-09-01 16:18:43 +0800

    init

---
+++ src/Website.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.disable('x-powered-by');
+
+app
+    .set('view engine', 'ejs')
+    .set('views', path.join(__dirname, 'views'))
+    .use(express.static(path.join(__dirname, 'public')))
+    .get('/', (req, res) => {
+        res.render('index.ejs');
+    })
+    .listen(3000, () => console.log('[Website Status]: voided.pw is online'));
